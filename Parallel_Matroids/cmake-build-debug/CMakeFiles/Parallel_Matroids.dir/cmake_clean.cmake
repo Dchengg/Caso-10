7cd38f48d0fdb30175701b53c26b1255599e520a
Diego Cheng Gómez <diegocheng1973@gmail.com>
2019-10-26 15:57:47 -0600

    Generic linked list added

---
+++ Parallel_Matroids/cmake-build-debug/CMakeFiles/Parallel_Matroids.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Parallel_Matroids.dir/main.c.o"
+  "Parallel_Matroids.pdb"
+  "Parallel_Matroids.exe"
+  "libParallel_Matroids.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Parallel_Matroids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
